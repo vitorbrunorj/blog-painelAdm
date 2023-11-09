62199223604d67b1ea4e453a07ef06f11cdd96f1
Vitor Bruno <vitorbrunorj@gmail.com>
2023-11-09 10:22:52 -0300

    Crianda a configuração do TinyMCE

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
 
 router.get('/articles', (req, res) => {
   res.send('Rotas de artigos');
 });
 
 router.get('/admin/articles/new', (req, res) => {
+  Category.findAll().then(categories => {
+    res.render('admin/articles/new', { categories: categories });
+  });
   res.send('Rotas para criar uma novo artigo');
 });
 
